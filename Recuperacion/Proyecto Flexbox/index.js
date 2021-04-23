3f32dd736c77e5da3054b673c9684d3ad1eaf0bd
francis1641 <francis1641@users.noreply.github.com>
2021-04-23 10:52:08 +0200

    Segundo index y style

--- Recuperacion/Proyecto Flexbox/index.js
+++ Recuperacion/Proyecto Flexbox/index.js
@@ -1,4 +1,10 @@
-const hamburguerContainer = document.querySelector("#header__main-nav");
-const hamburger = document.querySelector("-header__main-nav--hamburger");
-const link = document.querySelectorAll(".header__,main-link . . . . .");
+const hamburgerContainer = document.querySelector("#header__main-nav");
+const hamburger = document.querySelector(".header__main-nav--hamburger");
+const links = document.querySelectorAll(".header__main-nav--links li");
 
+hamburger.addEventListener("click", () => {
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
